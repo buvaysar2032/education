68c8a5f0ea297589c9671db46442a4c0168f3bc8
Fatima <kataev19821>
2022-03-28 01:48:23 +0300

    flowers

--- js/tabs.js
+++ js/tabs.js
@@ -1,19 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.tabs__btn').forEach(function(tabsBtn) {
-        tabsBtn.addEventListener('click', function(event) {
-          const path = event.currentTarget.dataset.path
-    
-          document.querySelectorAll('.tab-content').forEach(function(tabContent) {
-              tabContent.classList.remove('tab-content-active')
-          })
-          document.querySelectorAll('.tabs__btn').forEach(function (tabsBtn) {
-            tabsBtn.classList.remove('tabs__btn--default')
-          })
-          document.querySelector(`[data-target="${path}"]`).classList.add('tab-content-active')
-          this.classList.add('tabs__btn--default');
-     
+document.querySelector('#a_background').addEventListener('change', function () {
+    console.log('tab')
+    console.log(document.querySelectorAll('.tabs__btn'))
+    document.querySelectorAll('.tabs__btn').forEach(function (tabsBtn) { // выполняет функцию для каждого элемента
+        console.log(tabsBtn) // одну и туже функцию мы выполняем для каждой кнопки (option) с классом tabs__btn
+        // Каждый раз когда у tabsBtn происходит событие change вызывается функция у которой есть свой специальный
+        // объект event
+        tabsBtn.addEventListener('change', function (event) {
+            const path = event.currentTarget.dataset.path
+            console.log(path) // почему-то у меня ничего не выводится в консоль))
+        })
     })
-    
-    });
-  
-  });
+})
