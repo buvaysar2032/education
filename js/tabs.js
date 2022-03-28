71128ad234dfa562f171c8829982f379fe757b00
Fatima <kataev19821>
2022-03-28 16:04:01 +0300

    flowers

--- js/tabs.js
+++ js/tabs.js
@@ -1,13 +1,6 @@
-document.querySelector('#a_background').addEventListener('change', function () {
-    console.log('tab')
-    console.log(document.querySelectorAll('.tabs__btn'))
-    document.querySelectorAll('.tabs__btn').forEach(function (tabsBtn) { // выполняет функцию для каждого элемента
-        console.log(tabsBtn) // одну и туже функцию мы выполняем для каждой кнопки (option) с классом tabs__btn
-        // Каждый раз когда у tabsBtn происходит событие change вызывается функция у которой есть свой специальный
-        // объект event
-        tabsBtn.addEventListener('change', function (event) {
-            const path = event.currentTarget.dataset.path // переменная path его значение берется из события change
-            console.log(path) // ничего не выводится в консоль))
-        })
-    })
-})
+const select = document.querySelector('select');
+
+    select.onchange = () => {
+      tabs.querySelector('.active').classList.remove('active');
+      tabs.querySelectorAll('.tab')[select.options.selectedIndex].classList.add('active');
+    }
